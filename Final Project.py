d0f5c2af614090701901b5f2b1fdfedf37f3682b
coreyah <48022386+coreyah@users.noreply.github.com>
2019-03-12 21:51:08 -0400

    Update Final Project.py
    Add Archery Scoring System

--- Final Project.py
+++ Final Project.py
@@ -12,8 +12,7 @@ print('\n', "He asks if you would be wanting to follow in his footsteps.", '\n')
 answer = input("Will you accept the quest of becoming the Middle Kingdom's bravest soldier? ")
 
 if answer == ("no"):
-    print(
-        "You have brought dishonor to your family due to not accepting your father's offer. A new soldier emerges, however he is not of your family bloodline.")
+    print("You have brought dishonor to your family due to not accepting your father's offer. A new soldier emerges, however he is not of your family bloodline.")
     print("Your family shames you for not accepting the offer. ")
     try_again = input("Would you like too go back and make a different decision? ")
     if try_again == ("yes"):
@@ -29,21 +28,32 @@ if answer == ("yes"):
 
 # This code starts you off with the first day of training, and you need to input how you did. This code will become a new branch, as every layer will become a new branch at some point.
 
-    while True:
-        print('\n',"Because you are in the Han dynasty, you need to remember to not back down from fighting. If you fail with training, you fail those who expect greatness in you.")
-        print('\n', "You are first taught swordfighting. After many sessions, you are told how you did.",'\n')
-        grade = input("How did you do? Print poor/good/: ")
-        if grade == ("poor"):
-            print("You need to try harder!")
-            continue
-        elif grade == ("good"):
-            print('\n',"Good job!")
-            break
-        else:
-            print("Invalid input. Please say good, great, or poor.",'\n')
-            continue
+    print('\n',"Because you are in the Han dynasty, you need to remember to not back down from fighting. If you fail with training, you fail those who expect greatness in you.")
+    print('\n', "You are first taught swordfighting. After many sessions, you are told how you did.",'\n')
+    grade = input("How did you do? Print poor/good/: ")
+    if grade == ("poor"):
+        print("You need to try harder!")
+        continue
+    elif grade == ("good"):
+        print('\n',"Good job!")
+        break
+    else:
+        print("Invalid input. Please say good, great, or poor.",'\n')
+        continue
 
 # This if statement is made to clear out extra indents, and makes sure you are on the good path of the story.
 
 if answer == ("yes") and grade == ("good"):
-    print("Test")
+    print("Good job! After a few days of swordfighting, your group moves on to archery.",'\n')
+    print("You start off with 4 targets each on top of each other. Put in 4 scores between 0 and 100.",'\n')
+    sum = 0
+    for i in range(4):
+        score = int(input("Enter a score between 0 and 100: ")
+        if score > 100:
+            print("That score is not achievable. Stay in the range of 0 to 100.",'\n')
+        elif score >= 0 and score < 101:
+        sum = sum + score
+    print("Your total is " + str(sum).")
+
+    
+    
